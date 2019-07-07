c5b75d4a51c9ab4827da3de3bcc3a9ae6173bab3
Grom9941 <skv.41@ya.ru>
2019-07-07 23:15:29 +0300

    read line by line

--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -10,13 +10,14 @@ public class ReadFile {
         try {
             List<String> allLines = Files.readAllLines(Paths.get("src/main/resources/myFile.json/"));
             int currentNumber = 0;
+
+            Manage manage = new Manage();
+            Handler handler = new Handler();
+            Stack<String> stack = new Stack<>();
+
             for (String line : allLines) {
                 currentNumber++;
                 //give to DefaultHendler for manage sax
-                Manage manage = new Manage();
-                Handler handler = new Handler();
-                Stack<String> stack = new Stack<>();
-
                 manage.manageLineByLine(line, currentNumber, allLines.size(), handler, stack);
                 //System.out.println(line);
             }
