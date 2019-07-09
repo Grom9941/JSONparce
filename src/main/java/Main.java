54ce687a38c3b70d0c9d5c313dac0f69775cc9e5
Grom9941 <skv.41@ya.ru>
2019-07-09 19:50:43 +0300

    start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        List<Employee> zz = ReadFile.parceDoing();
+        System.out.println(zz);
+    }
 }
