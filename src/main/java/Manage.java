fc6addbd850abd044c8686459c919d18039b6348
Grom9941 <skv.41@ya.ru>
2019-07-09 19:50:56 +0300

    sax manage

--- src/main/java/Manage.java
+++ src/main/java/Manage.java
@@ -1,71 +1,90 @@
 import java.util.Stack;
 
 class Manage {
-    public void manageLineByLine(String currentLine, int currentNumberLine, int allNumberLine, Handler handler, Stack<String> stack) {
+
+    private static String[] splitToArray(String currentLine) {
+        String[] decomposed1 = currentLine.split(" ");
+        int j = 0;
+        while (decomposed1[j].equals("")) j++;
+        String currentLineWithoutSpace = currentLine.substring(j);
+
+        return currentLineWithoutSpace.split(": ");
+    }
+
+    private static void pushInStack(String decomposed, Stack<String> stack) {
+        if (decomposed.charAt(0) == '"') {
+            stack.push(decomposed.substring(1, decomposed.length() - 1));
+        } else {
+            stack.push(decomposed);
+        }
+    }
+
+    private static boolean checkEndElement(char decomposed) {
+        boolean endElement = false;
+        if (!(decomposed == '{'))
+            endElement = true;
+        if (decomposed == '[')
+            endElement = false;
+        return endElement;
+    }
+
+    private static int forSubstring(String decomposed) {
+        int endSecondString = 0;
+
+        if (decomposed.charAt(0) == '"') {
+            endSecondString = 3;
+        } else if (decomposed.charAt(decomposed.length() - 1) == ',') {
+            endSecondString = 1;
+        }
+
+        return endSecondString;
+    }
+
+    private static void startOrEndDocument(int currentNumberLine, int allNumberLine, Handler handler) {
+        if (currentNumberLine == 1) {
+            handler.startDocument();
+        }
+        if (currentNumberLine == allNumberLine) {
+            handler.endDocument();
+        }
+    }
+
+    void manageLineByLine(String currentLine, int currentNumberLine, int allNumberLine, Handler handler, Stack<String> stack) {
 
         boolean endElement = false;
 
         if (currentNumberLine == 1 || currentNumberLine == allNumberLine) {
-            if (currentNumberLine == 1) {
-                handler.startDocument();
-            }
-            if (currentNumberLine == allNumberLine) {
-                handler.endDocument();
-            }
+
+            startOrEndDocument(currentNumberLine, allNumberLine, handler);
+
         } else {
-            String[] decomposed1 = currentLine.split(" ");
-            int j = 0;
-            while (decomposed1[j].equals("")) j++;
-            String currentLineWithoutSpace = currentLine.substring(j);
+            String[] decomposed = splitToArray(currentLine);
 
-            String[] decomposed = currentLineWithoutSpace.split(": ");
-
-            if (decomposed[0].equals("{")) {
+            if (decomposed[0].equals("{"))
                 stack.push(decomposed[0]);
-            }
+
             if (decomposed[0].equals("}") || decomposed[0].equals("]"))
                 endElement = true;
 
             if (decomposed[0].charAt(0) == '"') {
 
                 int startSecondString = 0;
-                int endSecondString = 0;
-                if (decomposed[1].charAt(0) == '"') {
+                if (decomposed[1].charAt(0) == '"')
                     startSecondString = 1;
-                    endSecondString = 3;
-                } else {
-                    if (decomposed[1].charAt(decomposed[1].length()-1) == ',')
-                        endSecondString = 1;
-                }
-                if (decomposed[1].equals("[")) {
-                    startSecondString = 0;
-                    endSecondString = 0;
-                }
-                int endSubstringName = decomposed[0].length() - 1;
-                int endSubstringAttribute = decomposed[1].length() - endSecondString;
 
-                //String s1 = decomposed[0].substring(1, endSubstringName);
-                //String s2 = decomposed[1].substring(startSecondString, endSubstringAttribute);
+                int endSubstringName = decomposed[0].length() - 1;
+                int endSubstringAttribute = decomposed[1].length() - forSubstring(decomposed[1]);
 
                 handler.startElement(decomposed[0].substring(1, endSubstringName), decomposed[1].substring(startSecondString, endSubstringAttribute));
                 handler.characters(decomposed[1].toCharArray(), startSecondString, endSubstringAttribute);
-                if (decomposed[0].charAt(0) == '"') {
-                    stack.push(decomposed[0].substring(1, decomposed[0].length() - 1));
-                } else {
-                    stack.push(decomposed[0]);
-                }
 
-                if (!(decomposed[1].charAt(0) == '{'))
-                    endElement = true;
-                if (decomposed[1].charAt(0) == '[')
-                    endElement = false;
+                pushInStack(decomposed[0], stack);
+                endElement = checkEndElement(decomposed[1].charAt(0));
             }
 
             if (decomposed[0].equals("},") || endElement) {
                 handler.endElement(stack.pop());
-                //handler.endElement(decomposed[0].substring(1, endSubstringName));
             }
         }
-
     }
 }
